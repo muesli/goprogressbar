03a630d52bb84ead4fc505192595db8d575a4e0f
Christian Muehlhaeuser <muesli@gmail.com>
2017-08-05 16:08:32 +0200

    Removed dupe test

--- terminal_test.go
+++ terminal_test.go
@@ -28,15 +28,3 @@ func TestCursorMovement(t *testing.T) {
 	}
 	buf.Reset()
 }
-
-func TestPercentageSpecialValues2(t *testing.T) {
-	p := ProgressBar{Current: 0, Total: 0}
-	if p.percentage() != 1 {
-		t.Errorf("percentage should be 1 when both current and total are 0, got: %f", p.percentage())
-	}
-
-	p = ProgressBar{Current: 100, Total: 0}
-	if p.percentage() != 0 {
-		t.Errorf("percentage should be 0 when current is greater than 0 but the total is unknown (0), got: %f", p.percentage())
-	}
-}
